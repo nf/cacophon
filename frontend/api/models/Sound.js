1281e183af5d24efff5c730845a4e3364ea9e763
Johan Euphrosine <proppy@google.com>
2014-03-12 17:24:33 -0700

    frontend: call to backend on beforeCreate to save base64 encoded mp3

--- frontend/api/models/Sound.js
+++ frontend/api/models/Sound.js
@@ -2,18 +2,44 @@
  * Sound
  *
  * @module      :: Model
- * @description :: A short summary of how this model works and what it represents.
+ * @description :: Store a cacophon sound.
  * @docs		:: http://sailsjs.org/#!documentation/models
  */
+
+var request = require('request');
+var qs = require('querystring');
 
 module.exports = {
 
   attributes: {
-  	
+
   	/* e.g.
   	nickname: 'string'
   	*/
-    
-  }
+        a: 'integer',
+        b: 'integer',
+        c: 'integer',
+        mp3: 'string'
+  },
 
+  beforeCreate: function(values, next) {
+    var params = qs.stringify({
+      a: values.a / 100.0,
+      b: values.b / 100.0,
+      c: values.c / 100.0
+    });
+    var url = 'http://localhost:8080/audio?' + params;
+    request({
+      method: 'GET',
+      url: url,
+      encoding: null
+    }, function(err, response, body) {
+      if (err) {
+        console.error('mp3 encoding failed:', err);
+      } else {
+        values.mp3 = body.toString('base64');
+      }
+      next();
+    });
+  }
 };
