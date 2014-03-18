9c261e18686bffd01bad355f3a620bfb8b5448e1
Johan Euphrosine <proppy@google.com>
2014-03-18 16:53:20 -0700

    frontend: add websocket discovery and health check handler

---
+++ frontend/api/services/MetadataService.js
@@ -0,0 +1,26 @@
+var request = require('request');
+
+module.exports = {
+  get: function(key, cb) {
+    var url = 'http://metadata/computeMetadata/v1' + key;
+    request({
+      method: 'GET',
+      url: url,
+      headers: {
+        'X-Google-Metadata-Request': 'True'
+      }
+    }, function(err, response, body) {
+      if (err || response.statusCode != 200) {
+        var error = {
+          message: 'metadata request failed',
+          statusCode: response.statusCode,
+          err: err
+        };
+        console.error('MetadataService.get', error);
+        cb(error, null);
+      } else {
+        cb(null, body);
+      }
+    });
+  }
+}
