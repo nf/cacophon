2f89192cdc1c32190fc69950bd0687351b8b3d45
Johan Euphrosine <proppy@google.com>
2014-03-24 04:53:01 -0700

    backend: dumber cache

--- backend/main_appengine.go
+++ backend/main_appengine.go
@@ -28,32 +28,18 @@ func getCache(req *http.Request) (audio []byte) {
 }
 
 func putCache(req *http.Request, audio []byte) {
-	toCache <- cacheItem{req, audio}
+	c := appengine.NewContext(req)
+	if err := memcache.Set(c, &memcache.Item{
+		Key:   cacheKey(req),
+		Value: audio,
+	}); err != nil {
+		c.Errorf("putCache: %v", err)
+	}
 }
 
 type cacheItem struct {
 	req   *http.Request
 	audio []byte
-}
-
-var toCache = make(chan cacheItem)
-
-func init() {
-	http.HandleFunc("/_ah/start", startHandler)
-}
-
-func startHandler(w http.ResponseWriter, r *http.Request) {
-	c := appengine.NewContext(r)
-	for item := range toCache {
-		go func(req *http.Request, audio []byte) {
-			if err := memcache.Set(c, &memcache.Item{
-				Key:   cacheKey(req),
-				Value: audio,
-			}); err != nil {
-				c.Errorf("putCache: %v", err)
-			}
-		}(item.req, item.audio)
-	}
 }
 
 func cacheKey(req *http.Request) string {
